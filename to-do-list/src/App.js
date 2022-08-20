1b469cae7df268680f14ff0e072a2f579f5e0603
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2022-08-20 23:53:02 +0530

    first half structure

---
+++ to-do-list/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello world <code></code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          amaan is here to teach you react js  , 
+          click to continue
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
